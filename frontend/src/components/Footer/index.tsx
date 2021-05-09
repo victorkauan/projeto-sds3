bdd6b36904502f60601baedd48f489fa084de9dd
VictorKauan <victorkauan.alu.lmb@gmail.com>
2021-05-09 01:50:14 -0300

    Security config

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="footer mt-auto py-3 bg-dark">
       <div className="container">
-        <p className="text-light">App desenvolvido por <a href="https://github.com/victorkauan" target="_blank" rel="noreferrer">Victor Kauan</a></p>
+        <p className="text-light">App desenvolvido por <a href="https://github.com/victorkauan" target="_blank" rel="noreferrer"><strong>Victor Kauan</strong></a></p>
         <p className="text-light"><small><strong>Semana Spring React</strong><br />
       Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
       </div>
